26b33539d392825b0214b91f4c3a005f7a1fe8ec
Andrew McDermott <amcdermo@redhat.com>
2020-05-11 12:58:58 +0100

    Move certgen to its own package

---
+++ test/extended/router/certgen/cmd/certgen.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/openshift/origin/test/extended/router/certgen"
+)
+
+func main() {
+	validFor := 100 * time.Hour * 24 * 365 // 100 years
+	notBefore := time.Now()
+	notAfter := notBefore.Add(validFor)
+
+	crt, key, err := certgen.GenerateKeyPair(notBefore, notAfter, flag.Args()...)
+
+	if err != nil {
+		log.Fatalf("failed to generate key pair: %v", err)
+	}
+
+	s1, err := certgen.MarshalKeyToDERFormat(key)
+	if err != nil {
+		log.Fatalf("failed to marshal key: %v", err)
+	}
+
+	s2, err := certgen.MarshalCertToPEMString(crt)
+	if err != nil {
+		log.Fatalf("failed to marshal crt: %v", err)
+	}
+
+	fmt.Print(s1, "\n", s2)
+}
